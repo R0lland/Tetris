8ea82dc3333fa4dad0f849faf69d11dd6cd079bd
Mateus Trombetta <mtrombetta@kokku.com.br>
2022-09-30 22:31:27 -0300

    Project commit

---
+++ Assets/Scripts/WallKickController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct WallKick {
+    public Vector2[] positionsToTest;
+}
+
+public class WallKickController : MonoBehaviour
+{
+    public static WallKickController Instance;
+
+    public WallKick[] wallKickGeneralClockwise;
+    public WallKick[] wallKickGeneralCounterClockwise;
+    public WallKick[] wallKickIClockwise;
+    public WallKick[] wallKickICounterClockwise;
+    public WallKick[] wallKickOnInstantiate;
+
+    private void Awake() {
+        Instance = this;
+    }
+}

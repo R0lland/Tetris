7c2eba3ddd05b1f9a3f77c529b57a909d51a5f6c
Mateus Trombetta <trombetta.mateus@gmail.com>
2022-09-30 22:45:47 -0300

    Adding project

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Script to manage HUD elements
+public class HUDManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _menu;
+    [SerializeField]
+    private GameObject _gameOver;
+    [SerializeField]
+    private GameObject _controls;
+    [SerializeField]
+    private Text _gameOverScore;
+    [SerializeField]
+    private Text _scoreTxt;
+
+    public void StartGame() {
+        _menu.SetActive(false);
+        SetScore(0);
+    }
+
+    public void ReturnMenu() {
+        _menu.SetActive(true);
+        _gameOver.SetActive(false);
+    }
+
+    public void SetScore(int score) {
+        _scoreTxt.text = score.ToString();
+    }
+
+    public void DisableGameOver() {
+        _gameOver.SetActive(false);
+    }
+
+    public void GameOver(int score) {
+        _gameOver.SetActive(true);
+        _gameOverScore.text = "Score: " + score.ToString();
+    }
+
+    public void OpenControls(bool open) {
+        _controls.SetActive(open);
+        GameManager.Instance.PauseGame(!open);
+    }
+
+}
